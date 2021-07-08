b97d5b5eb77f40c6ef433388854aa4fe4f727610
Andrii Kondysiuk <kondysiuka@gmail.com>
2021-07-08 23:44:54 +0300

    qwe

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const products = require("./products");
-const carts = require("./carts");
-const customers = require("./customers");
-
-module.exports = {
-  products,
-  carts,
-  customers,
-};
--- utils/validate/schemas/products.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-const productSchema = Joi.object({
-  name: Joi.string().min(2).required(),
-  price: Joi.number().min(0).required(),
-});
-
-
-module.exports = productSchema
