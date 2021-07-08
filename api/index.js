4a2578c66a82c2bde1cc2f542af5936a0d082de7
Andrii Kondysiuk <kondysiuka@gmail.com>
2021-07-08 23:15:32 +0300

    test

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const products = require("./products");
+const carts = require("./carts");
+const customers = require("./customers");
+
+module.exports = {
+  products,
+  carts,
+  customers,
+};
---
+++ data/products.js
@@ -0,0 +1,18 @@
+const { v4 } = require("uuid");
+
+const products = [
+  {
+    _id: v4(),
+    name: "iPhone X",
+    price: 34000,
+  },
+  {
+    _id: v4(),
+    name: "GeForce RTX 3900",
+    price: 90000,
+  },
+];
+
+module.exports = products;
+
+
